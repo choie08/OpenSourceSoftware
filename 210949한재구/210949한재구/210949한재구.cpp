74f1538012cbece81cf6d0d7a66c1facc0f49d41
choie08 <hanjaegu08@gmail.com>
2022-10-04 11:18:05 +0900

    Homework

---
+++ 210949한재구/210949한재구/210949한재구.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= 100; i++) {
+		if (i % N == 0)
+			cout << i << " ";
+	}
+
+	return 0;
+}
+
